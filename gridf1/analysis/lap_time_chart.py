777d9dabda58cdd2cd37f5ccc147f562dc683aa8
LuDom Media <52815977+LuDomMedia@users.noreply.github.com>
2024-06-20 19:11:43 +0200

    Update lap_time_chart.py
    Add new class-based functionality to lap time chart. All function-based code is now deprecated

--- gridf1/analysis/lap_time_chart.py
+++ gridf1/analysis/lap_time_chart.py
@@ -2,8 +2,11 @@ import fastf1.core
 
 import gridf1.core
 import gridf1.plotting.time_series_plot
+from gridf1.plotting.time_series_plot import TimeSeriesPlot
 
 
+# DEPRECATED
+'''
 def create_lap_time_chart(session: fastf1.core.Session, driver: gridf1.core.Driver, *additional_drivers: gridf1.core.Driver) -> None:
     lap_times = {driver.name: [driver.laps['LapNumber'], driver.laps['LapTime'],
                                driver.color]}  # Create dictionary with x-, y-data and driver color
@@ -27,3 +30,47 @@ def create_lap_time_chart(session: fastf1.core.Session, driver: gridf1.core.Driv
     gridf1.plotting.time_series_plot.create_time_series_plot(lap_times, "Lap Number", "Lap Time", title,
                                                              virtual_safety_car_laps, safety_car_laps,
                                                              session, True)
+'''
+
+
+class LapTimeChart(TimeSeriesPlot):
+    def __init__(self, title_session: fastf1.core.Session, driver: gridf1.core.Driver,
+                 legend: bool = False, mark_sc: bool = False, mark_vsc: bool = False, mark_yellow_flag: bool = False):
+        x_label = "Lap Number"
+        y_label = "Lap Time"
+        title = driver.name + " Lap Time Progression"
+        super().__init__(x_label, y_label, title, title_session, legend)
+        self.mark_laps(driver, mark_sc, mark_vsc, mark_yellow_flag)
+        self.add_driver(driver)
+
+    def add_driver(self, driver: gridf1.core.Driver):
+        self.create_plot_line(driver.laps['LapNumber'], driver.laps['LapTime'], driver.name, driver.color)
+
+    def mark_laps(self, driver: gridf1.core.Driver, mark_sc: bool = False, mark_vsc: bool = False, mark_yellow_flag: bool = False):
+        if mark_sc:
+            sc_label_added = False
+            for index, lap in driver.laps.iterrows():
+                if '4' in str(lap['TrackStatus']):
+                    if not sc_label_added:
+                        self.mark_x_axis(lap['LapNumber'], "#FF6A00", "Safety Car")
+                        sc_label_added = True
+                    else:
+                        self.mark_x_axis(lap['LapNumber'], "#FF6A00", "")
+        if mark_vsc:
+            vsc_label_added = False
+            for index, lap in driver.laps.iterrows():
+                if '6' in str(lap['TrackStatus']) or '7' in str(lap['TrackStatus']) or '56' in str(lap['TrackStatus']):
+                    if not vsc_label_added:
+                        self.mark_x_axis(lap['LapNumber'], "#A600A6", "Virtual Safety Car")
+                        vsc_label_added = True
+                    else:
+                        self.mark_x_axis(lap['LapNumber'], "#A600A6", "")
+        if mark_yellow_flag:
+            for index, lap in driver.laps.iterrows():
+                if '2' in str(lap['TrackStatus']):
+                    yellow_flag_label_added = False
+                    if not yellow_flag_label_added:
+                        self.mark_x_axis(lap['LapNumber'], "#FFFF00", "Yellow Flag")
+                        yellow_flag_label_added = True
+                    else:
+                        self.mark_x_axis(lap['LapNumber'], "#FFFF00", "")
