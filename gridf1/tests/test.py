672afa40f2731fe8d265db5f184acda44d1048b0
LuDom Media <52815977+LuDomMedia@users.noreply.github.com>
2024-06-20 19:14:02 +0200

    Update test.py
    Add adapt test functionality to class-based approach and add test lines for the new Speed Chart

--- gridf1/tests/test.py
+++ gridf1/tests/test.py
@@ -14,4 +14,23 @@ gas = gridf1.driver.Driver("GAS", session)
 rus = gridf1.driver.Driver("RUS", session)
 
 '''
-gridf1.analysis.lap_time_chart.create_lap_time_chart(session, ham, nor)
+#gridf1.analysis.lap_time_chart.create_lap_time_chart(session, ham, nor)
+#pd.set_option('display.max_rows', 500)
+#pd.set_option('display.max_columns', 500)
+'''
+for index, lap in ham.laps.iterrows():
+    print(lap.get_telemetry().head(3))
+    break
+'''
+
+#print(ham.laps.pick_fastest().get_telemetry().head(3))
+
+Ham_SC = gridf1.analysis.speed_chart.SpeedChart(session, ham, 0, legend=True)
+#Ham_SC.add_driver(nor, 1)
+#Ham_SC.set_title("Speed Analysis", session)
+Ham_SC.plot()
+
+#Ham_LTC = gridf1.analysis.lap_time_chart.LapTimeChart(session, ham, legend=True, mark_sc=True, mark_vsc=True, mark_yellow_flag=True)
+#Ham_LTC.add_driver(nor)
+#Ham_LTC.set_title("Lap Time Progression", session)
+#Ham_LTC.plot()
