60969da9b341254f0d31df81d9fec00ea62e6e97
LuDom Media <52815977+LuDomMedia@users.noreply.github.com>
2024-06-21 13:31:38 +0200

    Change add_driver to add_lap
    Changed method name add_driver to add_lap because the old name was misleading. It is also possible to add two laps from the same driver, therefore "add_lap" is better suited

--- gridf1/tests/test.py
+++ gridf1/tests/test.py
@@ -25,12 +25,12 @@ for index, lap in ham.laps.iterrows():
 
 #print(ham.laps.pick_fastest().get_telemetry().head(3))
 
-Ham_SC = gridf1.analysis.speed_chart.SpeedChart(session, ham, 0, legend=True)
-#Ham_SC.add_driver(nor, 1)
+#Ham_SC = gridf1.analysis.speed_chart.SpeedChart(session, ham, 0, legend=True)
+#Ham_SC.add_lap(nor, 1)
 #Ham_SC.set_title("Speed Analysis", session)
-Ham_SC.plot()
+#Ham_SC.plot()
 
-#Ham_LTC = gridf1.analysis.lap_time_chart.LapTimeChart(session, ham, legend=True, mark_sc=True, mark_vsc=True, mark_yellow_flag=True)
-#Ham_LTC.add_driver(nor)
-#Ham_LTC.set_title("Lap Time Progression", session)
-#Ham_LTC.plot()
+Ham_LTC = gridf1.analysis.lap_time_chart.LapTimeChart(session, ham, legend=True, mark_sc=True, mark_vsc=True, mark_yellow_flag=True)
+Ham_LTC.add_driver(nor)
+Ham_LTC.set_title("Lap Time Progression", session)
+Ham_LTC.plot()
